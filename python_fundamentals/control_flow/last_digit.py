9bcd1031010bac16b5009bb2e0f246604a4ec201
Lucas <14808@holbertonstudents.com>
2026-09-22 03:06:38 +0200

    fixed missing quote line

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -5,8 +5,8 @@ last_digit = number - 10 * int(number / 10)
 if last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit > 5:
-    print(f"Last digit of {number} is {last_digit}"
+    print(f"Last digit of {number} is {last_digit} "
             f"and is greater than 5")
 else:
-    print(f"Last digit of {number} is {last_digit}
+    print(f"Last digit of {number} is {last_digit} "
     f"and is less than 6 and not 0")
