297190ebdd71d7415f2ac47dea79252327ca3b0b
Lucas <14808@holbertonstudents.com>
2026-09-22 03:37:27 +0200

    created alphabet.py

---
+++ python_fundamentals/control_flow/alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+result = ""
+for letter in alphabet:
+    if letter != 'q' and letter != 'e':
+        result += letter
+print(result)
