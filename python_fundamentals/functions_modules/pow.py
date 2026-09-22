b2dbf6dea75fdc23743370b68a3a558fc92adb7e
Lucas <14808@holbertonstudents.com>
2026-09-22 12:02:20 +0200

    Add pow.py

---
+++ python_fundamentals/functions_modules/pow.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3
+""" module that computes a raised to the power of b manually."""
+
+
+def pow(a, b):
+    result = 1
+    for i in range(b):
+        result = result * a
+    return result
