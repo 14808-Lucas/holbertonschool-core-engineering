cd045d76a0c8fb2a6eaadd949839714d2bdaf6a4
Lucas <14808@holbertonstudents.com>
2026-09-22 12:16:02 +0200

    fixed the negative b issue to allow for 0.01 printing

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -4,6 +4,10 @@
 
 def pow(a, b):
     result = 1
+    if b < 0:
+        for i in range(-b):
+            result = result * a
+            return 1 / result
     for i in range(b):
         result = result * a
     return result
