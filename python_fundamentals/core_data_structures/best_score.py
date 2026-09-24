529aed224e879a12d3b07cbb1b335f32dd2de35b
Lucas <14808@holbertonstudents.com>
2026-09-24 23:55:58 +0200

    created best score py

---
+++ python_fundamentals/core_data_structures/best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that returns the key with the biggest integer value."""
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    best_key = None
+    best_value = None
+    for key in a_dictionary:
+        if best_value is None or a_dictionary[key] > best_value:
+            best_key = key
+            best_value = a_dictionary[key]
+    return best_key
